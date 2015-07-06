808cc7fa91ee2d27def3d62d054588eddc6dd402
Boombostak <smokejaguar3050@hotmail.com>
2015-07-06 13:58:23 -0600

    It was an animator setting! Unity 5!
    -Need to now redo the menu changes that were reverted.

--- WingsOfRadiance/Assets/InputFieldAutoSelect.cs
+++ WingsOfRadiance/Assets/InputFieldAutoSelect.cs
@@ -4,8 +4,26 @@ using UnityEngine.UI;
 
 public class InputFieldAutoSelect : MonoBehaviour {
 
+    public bool alwaysAutoSelect = true;
+    
     void OnEnable()
     {
-        this.GetComponent<InputField>().Select();
+        Select();
+    }
+
+    void Update()
+    {
+        if (this.GetComponent<InputField>().isFocused == false && alwaysAutoSelect == true)
+        {
+            Debug.Log("trying to autoselect");
+            Select();
+        }
+    }
+
+    public void Select()
+    {
+            this.GetComponent<InputField>().Select();
+            Debug.Log("called Select");
+            Debug.Log(this.GetComponent<InputField>());
     }
 }
