d14241fd7b0083f71de078401d693490d1a8124e
Boombostak <smokejaguar3050@hotmail.com>
2015-07-23 12:09:54 -0600

    Kdays july23

---
+++ WingsOfRadiance/Assets/AttractUI.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttractUI : MonoBehaviour {
+
+    public Animation anim1;
+    public Animation anim2;
+    
+    // Use this for initialization
+	void Start () {
+        anim1.playAutomatically = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (anim1.isPlaying==false)
+        {
+            anim2.Play();
+        }
+        if (anim2.isPlaying==false)
+        {
+            anim1.Play();
+        }
+	}
+}
